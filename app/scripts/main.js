b4d092b30decdf13f8c0a7278f0ce0601e8ee46b
Sounak Pal <talk2sounak@gmail.com>
2016-07-10 19:36:17 +0530

    Fix -- Completed table page and fix in previous page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -21,6 +21,37 @@
         $('.bird-eye_projects .each-item').on('click', function() {
             $(this).toggleClass('active');
         });
+
+        //triggering table clicks
+        $('.table .sorting').on('click', function(){
+            if($(this).hasClass('ascending')){
+              $(this).removeClass('ascending').addClass('descending');
+              //sorted descending
+            }else if($(this).hasClass('descending')){
+              $(this).removeClass('descending').addClass('ascending');
+              //sorted ascending
+            }else{
+              $(this).siblings('.ascending').removeClass('ascending');
+              $(this).siblings('.descending').removeClass('descending');
+              $(this).addClass('ascending');
+              //sorted ascending
+            }
+        });
+
+        $('.table .textarea').on('focus', function(){
+            var ele = $(this).closest('tr');
+            ele.addClass('hovered');
+        });
+
+        $('.table .textarea').on('blur', function(){
+            var ele = $(this).closest('tr');
+            ele.removeClass('hovered');
+        });
+
+        $('.table .clear').on('click',function(){
+            var ele = $(this).closest('td').find('textarea');
+            ele.val('').focus();
+        });
     };
     ui.checkStatusProject = function() {
         $('.bird-eye_projects .each-item').each(function() {
@@ -35,5 +66,8 @@
 
           ele.style.width = percent;
         });
-    }
+    };
+
+
+
 })(window, jQuery);
